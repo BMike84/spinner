ba4c864b74215d708b9e7c7219f0fbe2aa437eed
Michael Buffone <m_buffone@hotmail.com>
2022-02-16 19:03:58 -0500

    finsihed spinner2

---
+++ spinner2.js
@@ -0,0 +1,8 @@
+let spinnerString = ['|', '/', '-', '\\', '|', '/', '-', '\\', '|', '\n'];
+
+let delay = 0;
+for (const char of spinnerString) {
+  setTimeout(() => {
+    process.stdout.write(`\r${char}`);
+  },delay += 300);
+}
